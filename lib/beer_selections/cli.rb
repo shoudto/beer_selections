bb5afbf19d023f07ec547e498dcaf44b2066a7a0
Oscar Shoudt <shoudto@gmail.com>
2019-05-18 23:05:45 +0000

    added nokogiri and html

--- lib/beer_selections/cli.rb
+++ lib/beer_selections/cli.rb
@@ -1,7 +1,15 @@
 class Cli 
   
   def call
+  #  binding.pry 
+    puts "Welcome Beer Lovers!"
+    puts "Here's a list of Top Craft Beers!"
+    puts "      "
+    puts "------------------"
+    puts "      "
+    
+    html = open("https://www.beeradvocate.com/lists/top/")
+    doc = Nokogiri::HTML(html)
     binding.pry 
-    puts "Welcome to Top listed Craft Beers"
   end 
 end 
