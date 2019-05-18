3158a2d1ce887d75b321bdabc5bcd4a8a20ad156
Oscar Shoudt <shoudto@gmail.com>
2019-05-18 00:55:53 +0000

    added instances all variable

--- lib/beer_selections/beer.rb
+++ lib/beer_selections/beer.rb
@@ -2,8 +2,15 @@ class Beer
   
   attr_accessor :name, :type, :price, :url  
   
+  @@all = []
+  
   def initialize
-    @name = name 
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
   end 
   
 end
