d9ada0c65ef3bce5a9ab372c0bf409636a6fb2d5
Oscar Shoudt <shoudto@gmail.com>
2019-05-19 23:19:12 +0000

    calling the beer

--- lib/beer_selections/cli.rb
+++ lib/beer_selections/cli.rb
@@ -17,9 +17,11 @@ class Cli
       puts " "
       input = gets.strip
      # binding.pry 
-      Scraper.scrape_beer_details(Beer.all[input.to_i-1]) if !Beer.all[input.to_i-1].brewer 
+      Scraper.scrape_beer_details(Beer.all[input.to_i-1]) if !Beer.all[input.to_i-1].brewer
+       puts " "
        puts Beer.all[input.to_i-1].name
        puts Beer.all[input.to_i-1].company
+       puts Beer.all[input.to_i-1].style 
        puts " "
     end 
   end 
