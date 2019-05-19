8bd1820f83c3cc054ba39667618c693e991d9752
Oscar Shoudt <shoudto@gmail.com>
2019-05-19 05:42:31 +0000

    scraper method edited

--- lib/beer_selections/cli.rb
+++ lib/beer_selections/cli.rb
@@ -8,19 +8,7 @@ class Cli
     puts "------------------"
     puts "      "
     
-    
-    html = open("https://www.beeradvocate.com/lists/top/")
-    doc = Nokogiri::HTML(html)
-    
-      beer_list = doc.css("tr")
-      
-      beer_list.each do |beer|
-        
-       # link = beer.css('a').attr('href').value
-       
-       brew = Beer.new 
-       brew.name = beer.css("a").text
-    end
+    Scraper.scrape_beer_list
       binding.pry 
   end 
   
--- lib/beer_selections/scraper.rb
+++ lib/beer_selections/scraper.rb
@@ -2,6 +2,18 @@ class Scraper
   
   def self.scrape_beer_list
     
+    html = open("https://www.beeradvocate.com/lists/top/")
+    doc = Nokogiri::HTML(html)
+    
+      beer_list = doc.css("tr")
+      
+      beer_list.each do |beer|
+        
+       # link = beer.css('a').attr('href').value
+       
+       brew = Beer.new 
+       brew.name = beer.css("a").text
+    end
   end 
   
   def self.scrape_beer_details(beer)
