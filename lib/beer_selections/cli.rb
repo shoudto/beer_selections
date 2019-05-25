4c824028a56ceeb3b8315bcbd298bab990a83733
Oscar Shoudt <shoudto@gmail.com>
2019-05-25 00:47:16 +0000

    modifying cli class

--- lib/beer_selections/cli.rb
+++ lib/beer_selections/cli.rb
@@ -7,24 +7,31 @@ class Cli
     puts " "
     puts "------------------"
     puts " "
-    
     Scraper.scrape_beer_list
     print_beers
+    pick_your_craft
+  end 
+  
+  
+  def pick_your_craft
+    
     input = ""
     while input != "exit" do 
+      
       puts "Choose your appealing BEER! for more info?"
       puts "Enter a number or type 'exit' to exit."
       puts " "
+      
       input = gets.strip.downcase
       Scraper.scrape_beer_details(Beer.all[input.to_i-1]) if !Beer.all[input.to_i-1].company
       unless input == 'exit'
-       puts " "
-       puts "Beer: #{Beer.all[input.to_i-1].name}" # Beer.all[input.to_i-1].name
-       puts "--------------------------------------------------------------------------------"
-       puts "Beer Company: #{Beer.all[input.to_i-1].company}" # Beer.all[input.to_i-1].company
-       puts  "-------------------------------------------------------------------------------"
-       puts "Type of Beer: #{Beer.all[input.to_i-1].style }" # Beer.all[input.to_i-1].style 
-       puts " "
+      puts " "
+      puts "Beer: #{Beer.all[input.to_i-1].name}" # Beer.all[input.to_i-1].name
+      puts "--------------------------------------------------------------------------------"
+      puts "Beer Company: #{Beer.all[input.to_i-1].company}" # Beer.all[input.to_i-1].company
+      puts  "-------------------------------------------------------------------------------"
+      puts "Type of Beer: #{Beer.all[input.to_i-1].style }" # Beer.all[input.to_i-1].style 
+      puts " "
        
       end # end of the conditional unless
     end # end of while loop
